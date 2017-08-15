4e9c4d640b94c15409c6ea4d42e132668ce234e0
Robert Larkin <larkin.robertl@gmail.com>
2017-08-15 15:04:25 -0400

    add the zip command

--- package.sh
+++ package.sh
@@ -3,8 +3,9 @@
 echo $(pwd)
 echo $(ls)
 [[ -d ./src ]] || echo "Missing src dir"
-[ -d ./deploy ]] || echo "Missing deploy dir"
+[[ -d ./deploy ]] || echo "Missing deploy dir"
 [[ -d ./package ]] || echo "Missing package dir"
 
+zip -r ./package/src.zip ./src/*
 
 
